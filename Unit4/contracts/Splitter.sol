88b1b72d0abb75ac6bae6e4a97c290be72771f8f
Intoit82 <30686909+Intoit82@users.noreply.github.com>
2017-10-05 14:14:21 +0300

    Update Splitter.sol
    Fixing according to remark and changing the concept of the contract.
    I might get it wrong but if the only purpose is to allow a user to split funds - than no real need to regard anyone else other than Alice (or any other name).

--- Unit4/contracts/Splitter.sol
+++ Unit4/contracts/Splitter.sol
@@ -1,103 +1,54 @@
 pragma solidity ^0.4.8;
 
-import './StringUtils.sol';
-
 contract Splitter {
     
     //Addresses
-    address private splitterAddress;
-    address private receiver1;
-    address private receiver2;
     address private owner;
+     
     
-    event LogSetAddress(address setAddress, string name);
+    //log every fund transfer
     event LogFundsTransferred(string name, uint amount);
     
-    //Sets the owner
+    //Sets the owner for kill fuction
     function Splitter() public
     {
         owner = msg.sender;
     }
-    
-    function setAccountType(string typeOfAccount) 
-    public
-    returns (bool success)
-    {
-        success = false;
-        //make sure sender is not already defined
-        require(msg.sender != splitterAddress);
-        require(msg.sender != receiver1);
-        require(msg.sender != receiver2);
-        if (StringUtils.equal(typeOfAccount,"splitter") && splitterAddress == address(0))
-        {
-          splitterAddress = msg.sender;
-          success = true;
-         
-        }
-          
-        if (StringUtils.equal(typeOfAccount,"first") && receiver1 == address(0))
-        {
-            receiver1 = msg.sender;
-            success=true;
-        }
-          
-        if (StringUtils.equal(typeOfAccount,"second") && receiver2 == address(0))
-        {
-            receiver2 = msg.sender;
-            success = true;
-        }
-        
-        if (success)
-         LogSetAddress(msg.sender, typeOfAccount);
-        
-        return success;
-        
-          
-        
-    }
-    
-    function getBalance() 
-    public
-    constant
-    returns (uint)
-    {
-        return msg.sender.balance;
-    }
-    
-    function getContractBalance() 
-    public
-    constant
-    returns (uint)
-    {
-        return this.balance;
-    }
-    
-    function deposit()
+
+    //Allow to deposit funds and split it between two accounts
+    function splitDeposit(address firstReceiver ,address secondReceiver)
     public
     payable
     returns (bool)
     {
-        uint splitValue;
+        require(msg.value > 0); //check funds
+        require(firstReceiver != secondReceiver); //check the two addresses are different
+        require(firstReceiver != address(0)); //make sure address is not empty
+        require(secondReceiver != address(0)); //make sure address is not empty
+        require(firstReceiver != msg.sender); //make sure address is not sender
+        require(secondReceiver != msg.sender); //make sure address is not sender
         
-        if (msg.sender == splitterAddress)
-        {
-            require(receiver1 != address(0));
-            require(receiver2 != address(0));
+        
+        //get the first value for the split
+        uint splitValue =  msg.value /2;
             
-            splitValue =  msg.value /2;
-            if(receiver1.send(splitValue)) 
-               LogFundsTransferred("first",splitValue);
-               else throw;
+        if(firstReceiver.send(splitValue)) 
+           LogFundsTransferred("first",splitValue);
+        else throw; //no transaction occure
                 
-            if (receiver2.send(msg.value - splitValue))
-               LogFundsTransferred("second",msg.value - splitValue);
-               else throw;
+        if (secondReceiver.send(msg.value - splitValue))
+           LogFundsTransferred("second",msg.value - splitValue);
+        else revert(); //revert the first transaction as well
             
-        }
-        
-        return true;
+        return true; 
     }
     
-    
+    //Kill the contract
+    function kill()
+    {
+        require(owner == msg.sender);
+        return suicide(owner);
+        
+    }
     
 }
