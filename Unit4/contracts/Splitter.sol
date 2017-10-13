111b89c8368483252a9de7c37fc21a2d164df1e6
Intoit82 <30686909+Intoit82@users.noreply.github.com>
2017-10-13 11:55:46 +0300

    Update Splitter.sol
    Fixing deposit function to do calc only.
    Adding withdraw function.
    Checking destroy flag.
    Adding balance check per address.
    Removing redundant requires.

--- Unit4/contracts/Splitter.sol
+++ Unit4/contracts/Splitter.sol
@@ -1,54 +1,105 @@
+
 pragma solidity ^0.4.8;
 
 contract Splitter {
     
     //Addresses
     address private owner;
-     
     
+    //Init the destroyed flag
+    bool isContractDestoryed = false;
+    
+    //Holds the current balance for each address
+    mapping (address => uint) balances;
+     
     //log every fund transfer
-    event LogFundsTransferred(string name, uint amount);
+    event LogFundsTransferred(address receiver, uint amount);
+    event LogFundsDeposit(uint amountSplit1, uint amountSplit2);
     
     //Sets the owner for kill fuction
     function Splitter() public
     {
         owner = msg.sender;
     }
+    
+      //revent to avoid sinking ether on destoryed contract
+    modifier ReventDestoryed()
+    {
+        
+        if(isContractDestoryed)
+            revert();
+        _;
+    }
 
     //Allow to deposit funds and split it between two accounts
     function splitDeposit(address firstReceiver ,address secondReceiver)
     public
     payable
+    ReventDestoryed()
     returns (bool)
     {
         require(msg.value > 0); //check funds
-        require(firstReceiver != secondReceiver); //check the two addresses are different
-        require(firstReceiver != address(0)); //make sure address is not empty
-        require(secondReceiver != address(0)); //make sure address is not empty
         require(firstReceiver != msg.sender); //make sure address is not sender
         require(secondReceiver != msg.sender); //make sure address is not sender
         
         
         //get the first value for the split
         uint splitValue =  msg.value /2;
-            
-        if(firstReceiver.send(splitValue)) 
-           LogFundsTransferred("first",splitValue);
-        else throw; //no transaction occure
-                
-        if (secondReceiver.send(msg.value - splitValue))
-           LogFundsTransferred("second",msg.value - splitValue);
-        else revert(); //revert the first transaction as well
-            
+        
+        //update the funds 
+        balances[firstReceiver] += splitValue;
+        balances[secondReceiver] += msg.value - splitValue;
+        LogFundsDeposit(splitValue,msg.value - splitValue);
+        
         return true; 
+    }
+    
+    //get a single account value
+    function getAccountTotalValue(address checkAdd)
+    constant
+    public
+    returns (uint)
+    {
+        return balances[checkAdd];
+    }
+    
+    //Allow to users to request the funds
+    function withDrawFunds()
+    public
+    returns (bool)
+    {
+        //check the sender have any balance leftover
+       require(balances[msg.sender] >0);
+       
+       //save and init the balance to prevent re-entrancy attacks
+       var amount = balances[msg.sender];
+       balances[msg.sender] = 0;
+       
+       //do the transfer
+       if(!msg.sender.send(amount))
+        throw;
+       
+       //log the transfer
+       LogFundsTransferred(msg.sender,amount);
+       
+       return true;
+        
     }
     
     //Kill the contract
     function kill()
     {
+        isContractDestoryed = true;
         require(owner == msg.sender);
         return suicide(owner);
         
+    }
+  
+    
+    function()
+    ReventDestoryed()
+    {
+       
     }
     
 }
