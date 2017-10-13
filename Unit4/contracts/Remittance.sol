d808b2e8185dd0829d73b256842a1be6ccb7e9b0
Intoit82 <30686909+Intoit82@users.noreply.github.com>
2017-10-13 15:43:52 +0300

    Update Remittance.sol
    Creating a simple key mapping for the funds
    Changing the way the key is constructed:
    //Remark: How the key for accessing the remittanceStruct is constructed:
        //1. Sub1 = Hashing (Sender password + Recevier password)
        //2. Sub2 = Hashing (Sub1, exchange address)
        //3. Key = Hashing (Sub1 , Sub2)
    
    Changing all the functions accordingly.
    Adding flag for destruction
    Adding description to the security hole
    Add owner commission (I hope I got the goal of this exercise part correctly...)

--- Unit4/contracts/Remittance.sol
+++ Unit4/contracts/Remittance.sol
@@ -2,23 +2,33 @@ pragma solidity ^0.4.8;
 
 contract Remittance{
     
-    struct payerStruct
+    //Remark: How the key for accessing the remittanceStruct is constructed:
+    //1. Sub1 = Hashing (Sender password + Recevier password)
+    //2. Sub2 = Hashing (Sub1, exchange address)
+    //3. Key = Hashing (Sub1 , Sub2)
+    
+    //Struture for the funds remittance
+    struct remittanceStruct
     {
         uint amount;
-        bytes32 hashedPasswords;
         address recevierAddress;
+        address sender;
         uint timeOfDeposit;
         uint ownerCut;
         
     }
    
-    address public owner;
+    //save owner to allow contract destruction and to collect commissions
+    address private owner;
     
-    //holds the mappings of payers
-    mapping(address => payerStruct) payers;
+    //flag for destorying contract
+    bool private isContractDestoryed;
     
-    //holds list of Payers address array
-    address[] public listOfPayers;
+    //holds the balace for commissions
+    mapping (address => uint) public commissionBalance;
+    
+    //holds the mappings of payers by a key that unlocks the funds
+    mapping(bytes32 => remittanceStruct) private remittances;
     
     //set time limit as default
     uint public claimBackTimeLimit = 1 minutes;
@@ -28,6 +38,23 @@ contract Remittance{
     event LogOnCommitionCharge(address exchangeAdd,uint amount);
     event LogOnWithdrawal(address receiverAdd,uint amount);
     event LogOnClaimBack(address receiverAdd,uint amount,uint time);
+    
+    //revent to avoid sinking ether on destoryed contract
+    modifier ReventDestoryed()
+    {
+        
+        if(isContractDestoryed)
+            revert();
+        _;
+    }
+    
+    //Check is owner
+    modifier isOwner()
+    {
+        
+        require(msg.sender == owner);
+        _;
+    }
     
     //constructor
     function Remittance()
@@ -39,181 +66,193 @@ contract Remittance{
     //Sets the time limit
     function setTimeLimit(uint newTimeLimit)
     public
+    isOwner()
     returns (bool)
     {
-       require(msg.sender == owner);
        claimBackTimeLimit = newTimeLimit;
        return true;
     }
     
     //Set a deposit for payment method
-    function depositFundsForPayment
-    (bytes32 fundsPasswordHashed,bytes32 recevierPasswordHashed,address receiverAdd)
+    function depositFundsForPayment(bytes32 key,address receiverAddress)
     public
+    ReventDestoryed()
     payable
     returns (bool)
     {
         //check conditions
        require (msg.value >0 );
-       require (msg.sender != receiverAdd );
+       require (msg.sender != receiverAddress );
+       require(remittances[key].sender == address(0));
        
-       //hash the passwords in order to keep only a single hash in the contract
-       var passHash = keccak256(recevierPasswordHashed,fundsPasswordHashed);
+       //Remark: the key1 value is constructed from the hash of depositPassword + recevierPassword
+       //Then final the key is constructed from the hash of key1 + recevierAddress
        
        //set the values
-       payers[msg.sender].amount += msg.value;
-       payers[msg.sender].hashedPasswords = passHash;
-       payers[msg.sender].recevierAddress = receiverAdd;
-       payers[msg.sender].timeOfDeposit = now;
-       payers[msg.sender].ownerCut = msg.gas / 50;
+       remittances[key].amount += msg.value;
+       remittances[key].recevierAddress = receiverAddress;
+       remittances[key].timeOfDeposit = now;
+       remittances[key].ownerCut = msg.gas / 50;
+       remittances[key].sender = msg.sender;
        
        //log
        LogOnDeposit(msg.sender,msg.value,now);
-       
-       //add the payer to the list of payers
-       listOfPayers.push(msg.sender);
        
        return true;
        
     }
     
     //Withdraw funds from the contract
-    function withdrawFundsFromContract
-    (bytes32 fundsPassword,bytes32 recevierPassword)
+    //Funds password is the combined hash of both the deposit password and the revceiver password 
+    function withdrawFundsFromContract (bytes32 fundsPassword) 
     public
     returns (bool found)
     {
-        //check conditions
-        require (listOfPayers.length >0);
+       
+        //Create the single key that is expected to unlock the funds 
+        var key = keccak256(fundsPassword,msg.sender); //msg.sender should be only the exhange address
+        //key = fundsPassword; //for testing
         
-        //Create the single hash expected in the contract's storage
-        var fundHash = keccak256(fundsPassword);
-        var recevierHash = keccak256(recevierPassword);
-        var hashPass = keccak256(recevierHash,fundHash);
+        //Remark: the msg.sender address is being use to avoid an hacker to try to resend the same
+        // withdraw request with his own address
         
-        //find the passwords location in the mapping
-        uint i = 0;
-        found = false;
-        while (i <= listOfPayers.length && !found)
-        {
-            if(payers[listOfPayers[i]].hashedPasswords == hashPass)
-                found = true;
-            else
-                i++;
-        }
+        //check the generated key exist and has funds
+        require(remittances[key].amount >0);
         
-        //unlock funds if found
-        if (found)
-        {
-            //check conditions
-            var payer= payers[listOfPayers[i]];
-            require(payer.recevierAddress != address(0));
-            require(payer.amount != 0);
-            
-            //pay commition to the exchange
-            uint commition = payer.amount /10;
-            if(!msg.sender.send(commition))
-            throw;
-            LogOnCommitionCharge(msg.sender,commition);
-            
-             //pay commition to the owner
-            if(!msg.sender.send(payer.ownerCut))
-            revert(); //revert the entire process
-            LogOnCommitionCharge(owner,payer.ownerCut);
-            
-            var remainingFunds = payer.amount - commition - payer.ownerCut;
-            
-            //send the funds
-          if(!payer.recevierAddress.send(remainingFunds))
-            revert(); //revert the entire process
-          else
-          {
-              LogOnWithdrawal(payer.recevierAddress,remainingFunds);
-             if(!initPayerMapping(listOfPayers[i]))
-             revert(); //revert the entire process
-              
-          }
-        }
-          
         
+         var tempStruct = copyStruct(remittances[key]);
+         initKeyMapping(key); //init to avoid re enterance attack
+         
+         var commission = tempStruct.amount /10;
+         var ownerCommission = msg.gas /2;
+         var remainingFunds = tempStruct.amount - commission - ownerCommission;
+         
+           //send the funds
+          if(!tempStruct.recevierAddress.send(remainingFunds))
+            throw; //stop the entire process
+        
+         LogOnWithdrawal(tempStruct.recevierAddress,remainingFunds);
+         
+         //set commission balance
+         commissionBalance[msg.sender] +=commission;
+         LogOnCommitionCharge(msg.sender,commission);
+         
+         //set owner commision 
+         commissionBalance[owner] +=ownerCommission;
+         LogOnCommitionCharge(owner,ownerCommission);
+       
+         
+         return true;
+
+    }
+    
+    //copy struct values
+    function copyStruct(remittanceStruct toBeCopied)
+    private
+    returns (remittanceStruct)
+    {
+        remittanceStruct  newStruct;
+        newStruct.amount = toBeCopied.amount;
+        newStruct.recevierAddress = toBeCopied.recevierAddress;
+        newStruct.sender = toBeCopied.sender;
+        newStruct.timeOfDeposit = toBeCopied.timeOfDeposit;
+        newStruct.ownerCut = toBeCopied.ownerCut;
+        
+        return newStruct;
         
     }
     
-    //init the payers maaping at location payer
-    function initPayerMapping(address payer)
+   
+    
+     //Exchange will claim their commission 
+    function claimComissions()
+    public
+    returns (bool)
+    {
+        require(commissionBalance[msg.sender] >0);
+        var amount = commissionBalance[msg.sender];
+        commissionBalance[msg.sender] =0;
+        
+        //send back the funds
+       if (!msg.sender.send(amount))
+        throw;
+       
+       //log the event 
+       LogOnCommitionCharge(msg.sender,amount);
+       
+       return true;
+    }
+    
+    //sender will Claim back the funds 
+    function claimBack(bytes32 key)
+    public
+    returns (bool)
+    {
+       //check the sender is the owner of the funds
+       require(remittances[key].amount >0 && remittances[key].sender == msg.sender) ;
+       
+       //check time limit
+       require (remittances[key].timeOfDeposit + claimBackTimeLimit > now);
+     
+       var tempStruct = copyStruct(remittances[key]);
+       initKeyMapping(key); //init to avoid re enterance attack  
+        
+       //send back the funds
+       if (!msg.sender.send(tempStruct.amount))
+        throw;
+       
+       
+       //log the event
+       LogOnClaimBack(msg.sender,tempStruct.amount,now);
+           
+       return true;
+        
+    }
+    
+    //Payer will Claim back the funds 
+    function claimBackSecurityHole(bytes32 key)
+    public
+    returns (bool)
+    {
+        
+        //Assuming one of the passwords is the recevierPassword and the calim back
+        //A hacker can try to get the revceiver funds by:
+        //1. Wait for the sender to try to claim back the funds 
+        //2. copy the key the sender has sent to claimBack function
+        //3. Generate the key by the sender key + the revceiver password + the exhange address
+        //4. call the withdrawFundsFromContract function and get the funds.
+     
+       
+       return true;
+        
+    }
+    
+     //init the key maaping at the given location
+    function initKeyMapping(bytes32 key)
     private
     returns (bool)
     {
-        payers[payer].amount =0;
-        payers[payer].recevierAddress = address(0);
-        payers[payer].hashedPasswords = "0x0";
+       remittances[key].amount =0;
+       remittances[key].recevierAddress = address(0);
+       remittances[key].sender = address(0);
+       remittances[key].timeOfDeposit = 0;
+       
         
         return true;
-    }
-    
-    //Payer will Claim back the funds 
-    function claimBack(bytes32 fundsPassword,bytes32 recevierPassword )
-    public
-    returns (bool)
-    {
-       //check conditions
-       var hashPass = keccak256(recevierPassword,fundsPassword);
-       require(payers[msg.sender].hashedPasswords == hashPass);
-       require(payers[msg.sender].amount >0);
-       
-       //check time limit
-       if (payers[msg.sender].timeOfDeposit + claimBackTimeLimit < now)
-        throw;
-       
-       //send back the funds
-       if (!msg.sender.send(payers[msg.sender].amount))
-        throw;
-       else
-       {
-           LogOnClaimBack(msg.sender,payers[msg.sender].amount,now);
-           if(!initPayerMapping(msg.sender))
-             throw;
-           
-       }
-       
-       return true;
-        
-    }
-    
-    //Payer will Claim back the funds 
-    function claimBackSecurityHole(address claimer )
-    public
-    returns (bool)
-    {
-       //check conditions
-       require(payers[msg.sender].amount >0);
-       
-       //check time limit
-       if (payers[msg.sender].timeOfDeposit + claimBackTimeLimit < now)
-        throw;
-       
-       //send back the funds
-       if (!msg.sender.send(payers[msg.sender].amount))
-        throw;
-       else
-       {
-           LogOnClaimBack(msg.sender,payers[msg.sender].amount,now);
-           if(!initPayerMapping(msg.sender))
-             throw;
-           
-       }
-       
-       return true;
-        
     }
     
     function kill()
     public
     {
+        
         require(owner == msg.sender);
         return suicide(owner);
         
     }
+    
+    function()
+    ReventDestoryed()
+    public {}
     
     
     
