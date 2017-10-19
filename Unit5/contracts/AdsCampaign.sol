e873fb9331394e51b314839768494c904ea4da1b
Intoit82 <30686909+Intoit82@users.noreply.github.com>
2017-10-20 01:39:22 +0300

    Update and rename AdvertiserCampaigns.sol to AdsCampaign.sol
    Restructure

--- Unit5/contracts/AdsCampaign.sol
+++ Unit5/contracts/AdsCampaign.sol
@@ -1,20 +1,24 @@
 pragma solidity ^0.4.8;
 
+import "./TrustedViews.sol";
 import "./AdsCampaignFundable.sol";
 
 //Describes a bidding system within the blockchain between advertisers and publisher per location.
-contract AdvertiserCampaigns is AdsCampaignFundable {
+contract AdsCampaign is AdsCampaignFundable, TrustedViews {
 
         
     //Log events
     event LogNewAdCampaign(address sender,uint id, int setLatitude,int setLongitude, uint setDurationInMinutes,uint setOfferDuration,uint pricePerAd,bytes32 contentLink);
     event LogUpdateAdCampaign(address sender,uint id, int setLatitude,int setLongitude, uint setDurationInMinutes,uint setOfferDuration,uint pricePerAd);
     event LogStopCamgain(address sender,uint id);
+    event LogReducingClaimedAmount(address publisherAdd,address campaignAdd,uint calimAmount,uint approvedAmount);
+    event LogApprovedClaim(address publisherAdd,address campaignAdd,uint approvedAmount);
+    event LogWithdrawFunds(address publisherAdd,address campaignAdd,uint amount);
     event LogContractPauseSet(bool isPaused);
     
     //Holds the advertiser address - creator of the the Ads campaign 
     address public advertiserAddress;
-     
+    
     //Is this the advertiser
     modifier isAdvertiser()
     {
@@ -24,26 +28,29 @@ contract AdvertiserCampaigns is AdsCampaignFundable {
     
     
     //Default constractor set the campagin creator as advertiser
-    function AdvertiserCampaigns(address registeredAdvertiserAddress)
+    function AdsCampaign(address registeredAdvertiserAddress)
     public
     {
         advertiserAddress = registeredAdvertiserAddress;
     }
     
+    
+    
+    //TODO remove testingKey
     //Gets the campagin properties
     function getCampaignProperties(uint id)
     constant
     isAdvertiser
     public
     returns (bool isActive, uint balance,  int setLatitude,int setLongitude,
-            uint setDurationInMinutes,uint setOfferDuration,uint pricePerAd,bytes32 contentLink)
+            uint setDurationInMinutes,uint setOfferDuration,uint pricePerAd,bytes32 contentLink,bytes32 testingKey)
     {
        var key = keccak256(msg.sender,id);
        
        //check if the campaigns really active
        var isReallyActive = campaigns[key].offerDuration > now && campaigns[key].isActive;
        return (isReallyActive,campaigns[key].balance,campaigns[key].latitude,campaigns[key].longitude,campaigns[key].viewDuration,
-              campaigns[key].offerDuration,campaigns[key].pricePerAd,campaigns[key].contentLink);
+              campaigns[key].offerDuration,campaigns[key].pricePerAd,campaigns[key].contentLink,key);
     }
     
      //Gets the campagin properties
@@ -58,7 +65,7 @@ contract AdvertiserCampaigns is AdsCampaignFundable {
              ,campaigns[key].pricePerAd);
     }
     
-   
+    
     
     /*Creates a new advertisment campaign
     id - a uniqe number for each campaign for this advertiser
@@ -110,7 +117,7 @@ contract AdvertiserCampaigns is AdsCampaignFundable {
     //Update an existing advertisment campaign
     function updateExistingAdvertimentCampaign(uint id,int currentLatitude,int currentLongitude,uint newViewDuration,uint newOfferDuration,uint pricePerAd)
     public
-    isAdvertiser
+    isOwner
     running
     campaignExist(id)
     returns (bool)
@@ -132,6 +139,116 @@ contract AdvertiserCampaigns is AdsCampaignFundable {
         return true;
         
     }
+    
+    
+    //Upload to the blockchain new view data
+    //Minimum of 100 views per upload
+    function uploadNewViews(address publisherAddress,address setCampaignAddress,
+                            int setLatitude,int setLongitude, uint numberOfViews,uint setMinDuration)
+    isOwner
+    running
+    public
+    returns (bool)
+    {
+       
+        //check for valid imputs
+        require(numberOfViews >100);
+        require(setMinDuration >0);
+        
+         //create a unique key for the views
+        var key = keccak256(publisherAddress,setCampaignAddress);
+        
+        //check no such view is already define and active
+        require(views[key].exist == false);
+        
+        //set new values for the view
+        views[key].latitude = setLatitude;
+        views[key].longitude = setLongitude;
+        views[key].minViewDuration = setMinDuration;
+        views[key].numberOfViews = numberOfViews;
+        views[key].exist = true;
+        
+        //log
+        OnNewViewUpload(publisherAddress,setCampaignAddress,setLatitude,setLongitude,
+                        numberOfViews,setMinDuration);
+        
+        return true;
+    }
+    
+    //Claim Views from sucessfull published campaign (for publishers)
+    function claimViews(address publisherAddress,bytes32 campaignId)
+    running
+    isOwner
+    public
+    returns (bool)
+    {
+        
+        
+        var campaignBalance = campaigns[campaignId].balance;
+        
+        //check that balance for the campaigns
+        require(campaignBalance >0);
+        
+         //create a unique key for the views
+        var key = keccak256(publisherAddress,this);
+        
+        //Protect re- enterance attack
+        var viewNumberTemp = views[key].numberOfViews;
+        views[key].numberOfViews = 0;
+        
+        //check no such view is already define and active
+        require(views[key].exist == true);
+        require(viewNumberTemp >0);
+        require(views[key].minViewDuration >= campaigns[campaignId].viewDuration); //check views are with standrads defined by the campaign
+        
+        
+        var tokenAmount = viewNumberTemp * campaigns[campaignId].pricePerAd;
+        
+        //Set approved amount
+        if (tokenAmount > campaignBalance)
+        {
+          tokenAmount = campaignBalance;
+          LogReducingClaimedAmount(publisherAddress,this, viewNumberTemp * campaigns[campaignId].pricePerAd,tokenAmount);
+        }
+        
+        //set the balance
+        campaigns[campaignId].balance -= tokenAmount;
+        
+        //add the claimed tokens
+        publishersApprovedClaims[publisherAddress] += tokenAmount;
+        
+        //log
+        LogApprovedClaim(publisherAddress,this,tokenAmount);
+        
+        return true;
+        
+    }
+    
+    //Withdraw funds from approved claims (for publishers)
+    function withdrawFunds(address publisherAddress)
+    running
+    isOwner
+    public
+    returns (bool)
+    {
+        
+        //check for positive balance
+        require(publishersApprovedClaims[publisherAddress] >0);
+        
+        //handle re enterance attack
+        var amount = publishersApprovedClaims[publisherAddress];
+        publishersApprovedClaims[publisherAddress] = 0;
+        
+        //send the funds
+        require(publisherAddress.send(amount));
+        
+        //log
+        LogWithdrawFunds(publisherAddress,this,amount);
+        
+        return true;
+        
+    }
+    
     
     /* TODO needs to implement Pause mode for the campaign
     //Stops an existing advertisment campaign
