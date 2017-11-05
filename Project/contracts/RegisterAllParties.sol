efd12c8405d912821df93d45b32dad9ebbe2d261
Intoit82 <30686909+Intoit82@users.noreply.github.com>
2017-11-05 18:01:17 +0200

    Update RegisterAllParties.sol
    Adding black list support

--- Project/contracts/RegisterAllParties.sol
+++ Project/contracts/RegisterAllParties.sol
@@ -6,4 +6,31 @@ import "./RegisterTrustees.sol";
 
 //Describes Register process for all parties 
 contract RegisterAllParties is RegisterAdvertisers, RegisterPublishers, RegisterTrustees  {
+    
+    //Holds the black list for party registration
+    mapping (address => bool) blackList;
+    
+    event LogOnUpdateBlackList(address partyToUpdate, bool onOff);
+    
+    //Check party is not in the black list
+    modifier notInBlackList()
+    {
+        require(blackList[msg.sender] == false);
+        _;
+    }
+    
+    //Update the party flag in the blacklist
+    function updatePartyInBlackList(address party, bool setOnOff)
+    running
+    isOwner
+    public
+    returns(bool)
+    {
+        //update party flag in blackList
+        blackList[party] != setOnOff;
+        
+        LogOnUpdateBlackList(party,setOnOff);
+        
+        return true;
+    }
 }
